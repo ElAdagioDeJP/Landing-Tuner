d2e4af6f13c6a86b12577145e8931a6332fb9a79
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2025-06-29 20:02:04 -0400

    Afinando el repo

---
+++ LandingTuner/src/main/java/com/ElAdagioDeJP/LandingTuner/LandingTunerApplication.java
@@ -0,0 +1,13 @@
+package com.ElAdagioDeJP.LandingTuner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandingTunerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandingTunerApplication.class, args);
+	}
+
+}
